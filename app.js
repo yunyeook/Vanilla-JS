7b59b7470ab38bfb800fc3995a1ec319a9731514
yunyeook <dudhr17050@naver.com>
2024-09-11 21:11:01 +0900

    event
    addEventListener(type, listener)
    
    on'eventname' = listener
     => 잘 사용 안함...
    
    *event : 프로그래밍하는 시스템에서 발생하는 사건
    *event listener : 이벤트가 발생한 후 응답하는 함수
    
    window
    전역 객체로 브라우저 창 안에 존재하는 모든 요소의 최상위 객체이다.

--- app.js
+++ app.js
@@ -1,16 +1,41 @@
-const idName = document.getElementById('hihi'); //html요소에서 id가 title인 객체를 반환함.
-idName.innerText = 'got you'; // title의 태그사이의 텍스트 바꾸기
-
-const classNames = document.getElementsByClassName('hello');
-
-const querySelectors = document.querySelector('.hello');
-querySelectors.style.backgroundColor = 'red';
-
-const id1 = document.getElementById('hihi');
-const id2 = document.querySelector('#hihi'); //더 많이 사용!
-
-const selectorAll = document.querySelectorAll('.hello');
-
-for (let i in selectorAll) {
-  selectorAll[i].style.backgroundColor = 'blue';
+const h1 = document.querySelector('.hello');
+function handleH1Click() {
+  const currentColor = h1.style.color;
+  let newColor;
+  if (currentColor === 'blue') {
+    newColor = 'green';
+  } else {
+    newColor = 'blue';
+  }
+  h1.style.color = newColor;
 }
+
+function handleMouseEnter() {
+  // console.log('mouse is here');
+  h1.style.color = 'red';
+  h1.innerText = 'mouse is here!';
+}
+function handleMouseLeave() {
+  h1.style.color = 'black';
+  h1.innerText = 'mouse is gone~';
+}
+// title.addEventListener('click', handleH1Click()); => 작동안함 why? 콜백함수에서 ()를 빼야함!!
+
+h1.addEventListener('click', handleH1Click);
+h1.addEventListener('mouseenter', handleMouseEnter); //해당요소에 마우스 올라갔을때
+// title.addEventListener('mouseleave', handleMouseLeave);
+
+h1.onmouseleave = handleMouseLeave; // 별로 선호 하지 않음
+
+function handleWindowResize() {
+  document.body.style.backgroundColor = 'skyblue';
+}
+function handleWindowCopy() {
+  alert('no copy');
+}
+function handleWindowoffline() {
+  alert('no wifi');
+}
+window.addEventListener('resize', handleWindowResize);
+window.addEventListener('copy', handleWindowCopy);
+window.addEventListener('offline', handleWindowoffline);
