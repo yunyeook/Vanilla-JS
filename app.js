ebaf1338de09047c3ecc108a7af321dae2a891a5
yunyeook <dudhr17050@naver.com>
2024-09-12 16:44:47 +0900

    preventDefault()
    alert()는 일단 기본동작을 멈춘 후 경고창을 띄우고 ok누르면 기본동작이 실행됨.

--- app.js
+++ app.js
@@ -1,21 +1,9 @@
-/*
-<div>
-  <input />
-  <button> </button>
- </div > 
- */
-const loginForm = document.querySelector('#login-form'); //div태그 요소를 저장
-const loginInput = loginForm.querySelector('input'); //div 태그 안에있는 요소를 찾는거라 document안써도 됨.!!
+const link = document.querySelector('a');
+function clickLink(event) {
+  //alert는 모든 동작을 일단 멈춘다!! ok누르면 원래 동작 진행함.
 
-function onLoginSubmit(event) {
-  event.preventDefault();
   console.log(event);
+  event.preventDefault();
+  alert('click');
 }
-loginForm.addEventListener('submit', onLoginSubmit);
-
-// function clickCheckbox(event) {
-//   event.preventDefault();
-//   console.log(event);
-// }
-
-// loginForm.addEventListener('click', clickCheckbox);
+link.addEventListener('click', clickLink);
