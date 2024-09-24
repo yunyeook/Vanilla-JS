e9fc1431996b30694f5e5a643870657803a6200c
yunyeook <dudhr17050@naver.com>
2024-09-24 15:05:11 +0900

    배경화면 넣기

--- js/background.js
+++ js/background.js
@@ -1,9 +1,10 @@
 const images = ['bg1.jpg', 'bg2.jpg', 'bg3.jpg']; //이미지 파일명이랑 같아야함.
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
-
 const bgimage = document.createElement('img');
+const body = document.querySelector('body');
+body.style.backgroundImage = `url(image/${chosenImage})`;
 
-bgimage.src = `image/${chosenImage}`;
+// bgimage.src = `image/${chosenImage}`;
 
-document.body.appendChild(bgimage);
+// document.body.appendChild(bgimage);
